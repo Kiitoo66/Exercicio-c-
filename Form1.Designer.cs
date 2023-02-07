5952a6b87c9ff8b6b598c58083618bf0c4df1ff2
Rodrigo <Rodrigo@DESKTOP-S0QJHH5>
2023-02-07 11:40:42 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace exercicios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.White;
+            this.btnEnviar.Location = new System.Drawing.Point(96, 86);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "button1";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+
+            string correta = "palavra";
+            string digitada = textBox1.Text;
+
+            if (correta.Equals(digitada, StringComparison.OrdinalIgnoreCase))
+            {
+                // É a palavra certa - permitir que o usuário continue
+            }
+            else
+            {
+                // Não é a palavra certa - avisar o usuário
+            }
+            string cor = textBox1.Text;
+
+                switch (cor)
+                {
+                    case "vermelho":
+                        btnEnviar.BackColor = Color.Red;
+                        break;
+                    case "laranja":
+                        btnEnviar.BackColor = Color.Orange;
+                        break;
+                    case "amarelo":
+                        btnEnviar.BackColor = Color.Yellow;
+                        break;
+                    case "verde":
+                        btnEnviar.BackColor = Color.Green;
+                        break;
+                    case "azul":
+                        btnEnviar.BackColor = Color.Blue;
+                        break;
+                    case "roxo":
+                        btnEnviar.BackColor = Color.Purple;
+                        break;
+                    case "preto":
+                        btnEnviar.BackColor = Color.Black;
+                        break;
+
+                    default:
+                        btnEnviar.BackColor = Color.White;
+                        break;
+                
+            }
+
+        }
+
+
+            }
+
+        }
+    
+
+
+
+        
+    
+
+
+
